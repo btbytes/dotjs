d175037dcd00c6b3049c85d23cb972c588e60633
Pradeep Kishore Gowda <pradeep@btbytes.com>
2012-03-03 15:46:19 -0500

    first commit

---
+++ facebook.com.js
@@ -0,0 +1,9 @@
+//src: https://raw.github.com/gilesbowkett/dotjsfiles/master/facebook.com.js
+//
+$("#rightCol").hide();
+$("#pagelet_home_stream").css("width", "600px");
+$("groupSideNavWrapper").hide();
+$("#pagelet_apps_nav").hide();
+$(".fbDock").hide();
+$("<style type='text/css'>.fbDock{ display: none }</style>").appendTo("head");
+
