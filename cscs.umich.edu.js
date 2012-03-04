929cc80269b5dd7d0956d180a4b81cbed6c2c946
Pradeep Kishore Gowda <pradeep@btbytes.com>
2012-03-04 13:06:47 -0500

    cosma notebooks

---
+++ cscs.umich.edu.js
@@ -0,0 +1,7 @@
+var pathname = window.location.pathname;
+if (pathname.indexOf("crshalizi") > 0) 
+{
+    document.body.setAttribute("style", "color: #000; background-color: #fff; padding: 2em; margin: max-width: 52em; font-size: 14pt; font: medium sans-serif");
+
+
+}
