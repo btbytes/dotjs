60a3e3ccc1f4210538797b3d4b785d8073dccbb0
Pradeep Kishore Gowda <pradeep@btbytes.com>
2012-03-05 00:49:46 -0500

    newyorker

---
+++ newyorker.com.js
@@ -0,0 +1,6 @@
+// newyorker.com
+
+document.body.innerHTML= document.getElementById('content').innerHTML;
+document.body.setAttribute("style", "margin: 2em 2em auto; max-width: 52em; font-size: 11pt;");
+
+
