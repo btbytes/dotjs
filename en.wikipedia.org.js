fb2a63ce02943a598f2c78a6484982c1a3834c8d
Pradeep Kishore Gowda <pradeep@btbytes.com>
2012-03-05 00:54:45 -0500

    english wikipedia

---
+++ en.wikipedia.org.js
@@ -0,0 +1,6 @@
+// English Wikipedia
+
+document.body.innerHTML= document.getElementById('content').innerHTML;
+document.body.setAttribute("style", "background-color: #fff; margin: 2em 2em auto; font-size: 13pt;");
+
+
