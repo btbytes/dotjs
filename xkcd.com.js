b8971afb3181baa92bc92996f6278bfac3824754
Pradeep Gowda <pradeep@btbytes.com>
2012-03-05 09:34:03 -0500

    fix comment

--- xkcd.com.js
+++ xkcd.com.js
@@ -1,4 +1,4 @@
-// English Wikipedia
+// xkcd.com -- web comics
 
 document.body.innerHTML= document.getElementById('middleContent').innerHTML;
 document.body.setAttribute("style", "background-color: #fff; margin: 2em 2em auto; font-size: 11pt; ul {display: block;}");
