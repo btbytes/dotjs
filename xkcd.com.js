3ba6a48ab0d234662f55f410113c54c6ff80c6a1
Pradeep Kishore Gowda <pradeep@btbytes.com>
2012-03-05 01:01:06 -0500

    xkcd

---
+++ xkcd.com.js
@@ -0,0 +1,6 @@
+// English Wikipedia
+
+document.body.innerHTML= document.getElementById('middleContent').innerHTML;
+document.body.setAttribute("style", "background-color: #fff; margin: 2em 2em auto; font-size: 11pt; ul {display: block;}");
+
+
