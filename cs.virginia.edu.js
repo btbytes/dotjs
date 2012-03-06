72b5cc5a303c00113e11dfad8c69de64f5916a4d
Pradeep Gowda <pradeep@btbytes.com>
2012-03-06 16:48:32 -0500

    weimer

---
+++ cs.virginia.edu.js
@@ -0,0 +1,28 @@
+// http://www.cs.virginia.edu/~weimer/
+//
+//
+var pathname = window.location.pathname;
+if (pathname.indexOf("~weimer") > 0) {
+    loadjscssfile("http://test.btbytes.com/btbytes.css", "css") 
+}
+
+
+/* loadjscssfile source: 
+ * http://www.javascriptkit.com/javatutors/loadjavascriptcss.shtml 
+ */
+
+function loadjscssfile(filename, filetype){
+     if (filetype=="js"){ //if filename is a external JavaScript file
+           var fileref=document.createElement('script')
+                 fileref.setAttribute("type","text/javascript")
+                   fileref.setAttribute("src", filename)
+                    }
+      else if (filetype=="css"){ //if filename is an external CSS file
+            var fileref=document.createElement("link")
+                  fileref.setAttribute("rel", "stylesheet")
+                    fileref.setAttribute("type", "text/css")
+                      fileref.setAttribute("href", filename)
+                       }
+       if (typeof fileref!="undefined")
+             document.getElementsByTagName("head")[0].appendChild(fileref)
+}
