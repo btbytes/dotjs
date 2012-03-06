dbe638b5deded01b44ebba84fd12b25871ab7009
Pradeep Kishore Gowda <pradeep@btbytes.com>
2012-03-06 01:08:06 -0500

    mooove

--- cscs.umich.edu.js
+++
@@ -1,7 +0,0 @@
-var pathname = window.location.pathname;
-if (pathname.indexOf("crshalizi") > 0) 
-{
-    document.body.setAttribute("style", "color: #000; background-color: #fff; padding: 2em; margin: max-width: 52em; font-size: 14pt; font: medium sans-serif");
-
-
-}
